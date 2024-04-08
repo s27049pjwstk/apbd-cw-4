672a772ed1cf0663765389c99650d095d2244838
s27049 <s27049@pjwstk.edu.pl>
2024-04-08 12:42:07 +0200

    Initial + Animal.cs and Configuration.cs

---
+++ VeterinaryClinic/VeterinaryClinic/Animals/Animal.cs
@@ -0,0 +1,11 @@
+﻿namespace VeterinaryClinic.Animals;
+
+public class Animal
+{
+    private int Id { get; set; }
+    private string Kategoria { get; set; }
+    private string Name { get; set; }
+    private int Weight { get; set; }
+    private string Color { get; set; }
+    
+}
---
+++ VeterinaryClinic/VeterinaryClinic/Animals/Configuration.cs
@@ -0,0 +1,16 @@
+﻿namespace VeterinaryClinic.Animals;
+
+public static class Configuration {
+    public static void RegisterEndpointsForAnimals(this IEndpointRouteBuilder app) {
+        //... GET /api/v1/animals
+        app.MapGet("/animals", () => {
+            //...
+            //...
+            return TypedResults.Ok();
+        });
+        //... POST /api/v1/animals
+        //... GET /api/v1/animals/{id}
+        //... PUT /api/v1/animals/{id}
+        //... DELETE /api/v1/animals/{id}
+    }
+}
